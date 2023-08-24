79db4641c08e165db9a121426511e630bd6451a9
Virinas-code <alexcode228@gmail.com>
2023-08-24 18:00:45 +0200

    feat(pymodloader): Begin work on mod loader

--- src/pymodloader/__init__.py
+++ src/pymodloader/__init__.py
@@ -4,11 +4,16 @@ PyModLoader
 
 Load mods for :py:mod:`moddingpy`
 """
+from pathlib import Path
+
+from .load import RessourcesLoader
 
 
-def init() -> None:
+def init(ressources_path: Path) -> None:
     """
     Initialize the mod loader.
 
     Loads mods.
     """
+    loader: RessourcesLoader = RessourcesLoader(ressources_path)
+    return loader
--- src/pymodloader/load.py
+++ src/pymodloader/load.py
@@ -5,6 +5,7 @@ Load mods from path
 Handles the ``resources`` directory
 """
 from pathlib import Path
+from typing import Callable
 
 
 class RessourcesLoader:
@@ -17,3 +18,27 @@ class RessourcesLoader:
             Default: ``resources``.
         """
         self.path: Path = path
+
+    @staticmethod
+    def _check_dir(path: Path, ressource: str) -> None:
+        """Check if a path is a directory.
+
+        :param Path path: Directory.
+        :param str ressource: Ressource in the error message.
+        """
+        if not path.is_dir():
+            raise FileNotFoundError(f"{ressource} is not a directory")
+
+    def _load_ressource(self, name: str, callback: Callable[[Path], None]) -> None:
+        """Load ressources.
+
+        :param str name: Ressource to load.
+            Also the name of the ressource folder.
+        :param Callable[[Path], None] callback: Function to load the ressource
+        """
+        folder: Path = self.path / name
+        self._check_dir(folder, name)
+        for element in folder.iterdir():
+            if not element.is_dir() or element.suffix != ".zip":
+                raise TypeError(f"{name} {element.name} is not in a standard format")
+            callback(element)
